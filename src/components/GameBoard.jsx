ecc19020cf4eff3a90964838447e4ac0369aceed
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2024-06-18 18:47:59 +0530

    lifting state up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,14 +6,15 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard(){
+export default function GameBoard({onSelectSquare, activePlayerSymbol}){
   const [gameBord, setGameBord] = useState(initialGameBoard);
   function handleSelectSquare (rowIndex, colIndex){
     setGameBord((prevGameBoard) => {
       const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-      updatedBoard[rowIndex][colIndex] = 'X'
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol
       return updatedBoard;
     });
+    onSelectSquare();
   }
   return (
     <ol id="game-board">
