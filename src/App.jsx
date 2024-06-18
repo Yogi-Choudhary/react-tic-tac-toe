0a4abb86328f5322ae02d97c1dbef6858d2420ea
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2024-06-18 11:53:55 +0530

    splitting components & building reusable compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-
+import Playe from "./components/Playe"
 function App() {
   
 
   return (
-    <p>Coming soon...</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Playe name="Player 1" symbol="X"/>
+          <Playe name="Player 2" symbol="O"/>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   )
 }
 
---
+++ src/components/Playe.jsx
@@ -0,0 +1,11 @@
+export default function Playe ({name, symbol}){
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">{name}</span>
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button>Edit</button>
+    </li>
+  );
+}
