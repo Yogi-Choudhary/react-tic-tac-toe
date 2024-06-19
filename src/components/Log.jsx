20c5e264d0f470efa61b5cef85b3f8918803f224
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2024-06-19 12:54:18 +0530

    user not click multiple times a button

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,11 +1,11 @@
-export default function Log ({turns}){
+export default function Log({turns}){
   return (
-    <lo id="log">
+    <ol id="log">
       {turns.map((turn) => (
         <li key={`${turn.square.row}${turn.square.col}`}>
           {turn.player} selected {turn.square.row},{turn.square.col}
         </li>
       ))}
-    </lo>
+    </ol>
   );
 }
